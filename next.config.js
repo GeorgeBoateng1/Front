b55516ab83986600225a3619455a3982d3220718
GeorgeBoateng1 <156913699+GeorgeBoateng1@users.noreply.github.com>
2025-03-11 09:23:41 -0400

    Update next.config.js
    Added CORS

--- next.config.js
+++ next.config.js
@@ -7,9 +7,22 @@ const withTM = require("next-transpile-modules")([
   "@fullcalendar/timeline"
 ]);
 
-module.exports = withTM({
+const nextConfig = withTM({
   swcMinify: false,
   trailingSlash: true,
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          { key: 'Access-Control-Allow-Credentials', value: 'true' },
+          { key: 'Access-Control-Allow-Origin', value: 'https://test.annologic.com' }, // Allow requests from your backend domain
+          { key: 'Access-Control-Allow-Methods', value: 'GET,DELETE,PATCH,POST,PUT' },
+          { key: 'Access-Control-Allow-Headers', value: 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, Authorization' },
+        ],
+      },
+    ];
+  },
   env: {
     // DEV_API: 'http://localhost:3031',
     // HOST_API_KEY: 'http://localhost:3030',
@@ -31,3 +44,5 @@ module.exports = withTM({
     MAPBOX: ""
   }
 });
+
+module.exports = nextConfig;
